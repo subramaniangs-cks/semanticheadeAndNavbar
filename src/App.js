b7c61edbb6979fbe16076f2dc1c1082e5742ed3c
subramanian <subramaniangs.cks@gmail.com>
2020-07-03 13:21:37 +0530

    Adding semantic component to App js and semantic css cdn to index

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Header, Icon, Image, Menu, Segment, Sidebar } from 'semantic-ui-react'
 import './App.css';
 
 function App() {
+  const [visible, setVisible] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Sidebar.Pushable as={Segment}>
+      <Sidebar
+        as={Menu}
+        animation='push'
+        icon='labeled'
+        inverted
+        onHide={() => setVisible(false)}
+        vertical
+        visible={visible}
+        width='thin'
+      >
+        <Menu.Item as='a' className="menu-item">
+          <Icon name='home' />
+          Home
+        </Menu.Item>
+        <Menu.Item as='a'>
+          <Icon name='gamepad' />
+          Games
+        </Menu.Item>
+        <Menu.Item as='a'>
+          <Icon name='camera' />
+          Channels
+        </Menu.Item>
+      </Sidebar>
+
+      <Sidebar.Pusher>
+        <Segment basic>
+        <Menu fixed="top" className="top-menu">
+
+          <Menu.Item
+            className="no-border"
+            onClick = {() => setVisible(!visible)}
+          >
+            <Icon name="bars" />
+          </Menu.Item>
+      </Menu>
+          <Header as='h3'>Application Content</Header>
+          <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+        </Segment>
+      </Sidebar.Pusher>
+    </Sidebar.Pushable>
   );
 }
 
